215e7bbe562572e4736e81291a4507562bace95e
Hania Abdalla <64498492+haniaabdalla@users.noreply.github.com>
2022-07-20 18:50:51 +0200

    up

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,4 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-module.exports = {
-  devServer: {
-    disableHostCheck: true
-  }
-}
+
